9946317c7eba44ab66dead44c8cb9aeb20ccf0fb
Jason <nicholsonj863@gmail.com>
2016-08-15 20:24:03 -0400

    Finished video four

---
+++ WebstormProjects/colorGame/app/colorGame.js
@@ -0,0 +1,66 @@
+/**
+ * Created by jason on 8/15/16.
+ */
+
+var colors = generateRandomColors(6);
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.querySelector("#colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++){
+  //add initial colors to squares
+  squares[i].style.background = colors[i];
+
+  squares[i].addEventListener("click", function () {
+    //grab color of clicked square
+    var clickedColor = this.style.background;
+    //compare clicked square to pickedColor
+    if (clickedColor === pickedColor){
+      messageDisplay.textContent = "Correct!"
+      changeColors(clickedColor);
+      h1.style.background = clickedColor;
+    }else {
+      this.style.background = "#232323";
+      messageDisplay.textContent = "Try again!"
+    }
+  })
+}
+
+function changeColors(color) {
+  //loop through all squares
+  for(var i = 0; i < squares.length; i++) {
+    squares[i].style.background = color;
+  }
+}
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+function generateRandomColors(num) {
+  //make an array
+  var arr = [];
+  //add num random colors array
+  for(var i = 0; i < num; i++){
+    //getRandomColor and fill array
+    arr.push(randomColor());
+  }
+  //return array
+  return arr;
+}
+
+function randomColor() {
+  //pick a red from 0 to 255
+  var r = Math.floor(Math.random() * 256);
+  //pick a blue from 0 to 255
+  var g = Math.floor(Math.random() * 256);
+  //pick a green from 0 to 255
+  var b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
+}
