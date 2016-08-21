8809870e7456237ed9ad4bcb1608ba2870ac2580
Jason <nicholsonj863@gmail.com>
2016-08-21 18:35:48 -0400

    Finished functional logic

--- WebstormProjects/colorGame/app/colorGame.js
+++ WebstormProjects/colorGame/app/colorGame.js
@@ -1,14 +1,61 @@
 /**
  * Created by jason on 8/15/16.
  */
-
-var colors = generateRandomColors(6);
-
+var numSquares =6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+var numSquares;
+
+easyBtn.addEventListener("click", function () {
+  easyBtn.classList.add("selected");
+  hardBtn.classList.remove("selected");
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(var i = 0; i < squares.length; i++) {
+    if(colors[i]){
+      squares[i].style.background = colors[i];
+    }else{
+      squares[i].style.display = "none";
+    }
+  }
+  h1.style.background = "#232323";
+});
+
+hardBtn.addEventListener("click", function () {
+  hardBtn.classList.add("selected");
+  easyBtn.classList.remove("selected");
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(var i = 0; i < squares.length; i++) {
+      squares[i].style.background = colors[i];
+      squares[i].style.display = "block";
+    }
+});
+
+resetButton.addEventListener("click", function () {
+  //generate all new colors
+  colors = generateRandomColors(numSquares);
+  //pick a new random color from array
+  pickedColor = pickColor();
+  //change colorDisplay to match picked color
+  colorDisplay.textContent = pickedColor;
+  //change colors of square
+  for(var i = 0; i < squares.length; i++) {
+      squares[i].style.background = colors[i];
+  }
+  h1.style.background = "#232323";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -24,6 +71,7 @@ for(var i = 0; i < squares.length; i++){
       messageDisplay.textContent = "Correct!"
       changeColors(clickedColor);
       h1.style.background = clickedColor;
+      resetButton.textContent = "Play again?"
     }else {
       this.style.background = "#232323";
       messageDisplay.textContent = "Try again!"
